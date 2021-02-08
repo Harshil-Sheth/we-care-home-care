3040ee6945621854d8bf5bfb79c5c022db69d43e
Harshil Sheth <harshilshah29599@gmail.com>
2021-02-09 00:16:24 +0530

    add service items routes with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component.jsx";
+
+const Furnishing = () =>(
+  <div><h1>Furnishing</h1></div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/furnishing' component={Furnishing} />
+    </Switch>
     </div>
   );
 }
--- src/components/service/service.component.jsx
+++ src/components/service/service.component.jsx
@@ -11,28 +11,33 @@ class Service extends React.Component {
             sections: [{
                 title: 'Furnishing',
                 imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcTdzvmYy9e5BCt1S3OyE9o287dDyHXA_lww&usqp=CAU',
-                id: '1'
+                id: '1',
+                linkUrl: 'Furnishing'
             },
             {
                 title: 'Beautifying',
                 imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7639x3M_O1N5An2Y6CNEdWnwL6HLzUX2fzw&usqp=CAU',
-                id: '2'
+                id: '2',
+                linkUrl: ''
             },
             {
                 title: 'Cleaning',
                 imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmvnY15Nf4OM82CycatyuVX9zb8gqpNs3LAw&usqp=CAU',
-                id: '3'
+                id: '3',
+                linkUrl: ''
             },
             {
                 title: 'Flooring',
                 imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkPjyQEMrs6mtBfLbcWUCBfZR7V7Y-0UprzQ&usqp=CAU',
                 id: '4',
+                linkUrl: '',
                 size: 'large'
             },
             {
                 title: 'Painting',
                 imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6ccIHqSWQdc6lSoW4bixXyAmdAbU_xw3YXg&usqp=CAU',
                 id: '5',
+                linkUrl: '',
                 size: 'large'
             }]
         }
@@ -41,8 +46,8 @@ class Service extends React.Component {
     render(){
         return(
             <div className='service-menu'>
-            {this.state.sections.map(({title, imageUrl, id, size}) => (
-                <ServiceItem key={id} title={title} imageUrl={imageUrl} size={size}/> 
+            {this.state.sections.map(({ id, ...otherSectionProps}) => (
+                <ServiceItem key={id} {...otherSectionProps}/> 
             ))}
         </div>
         )
